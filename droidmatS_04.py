21f2b30705720d7fd9c7d69e0f1162adfb12c7cb
Nemethze <74901919+Nemethze@users.noreply.github.com>
2025-11-09 14:30:21 +0100

    Change file read from config.txt to config_04.txt

--- droidmatS_04.py
+++ droidmatS_04.py
@@ -15,7 +15,7 @@ GcookieList = []
 def readinFile():
     os.system("rm config_04.txt")
     os.system("wget https://raw.githubusercontent.com/Nemethze/droidmatpuller/refs/heads/main/config_04.txt")
-    with open("config.txt", "r") as file:
+    with open("config_04.txt", "r") as file:
         lines = file.readlines()
     with open("port.txt","r") as file2:
         lines2 = file2.readlines()
