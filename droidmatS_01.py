e20cca43b9739756da5cdf976fe3ae256ec63d1e
Nemethze <74901919+Nemethze@users.noreply.github.com>
2026-01-24 22:12:18 +0100

    Add skipped_sites variable to track skipped ads

--- droidmatS_01.py
+++ droidmatS_01.py
@@ -74,6 +74,7 @@ def openAd(maxAds, sleepTime):
     ads_opened = 0
     last_y = 0     
     skipped_ads = 0
+    skipped_sites = 0
     while skipped_sites < 2:
         if d(textContains="Szponzorált termékek").exists(timeout=2) or d(textContains="Szponzorált").exists(timeout=2):
             sponsored_present = True
